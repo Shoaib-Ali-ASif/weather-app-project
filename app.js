cf0b9a39bd8e2f59a72bc0c466df415397284cd6
shoaib-ali-asif <eagleshebi@gmail.com>
2023-06-18 21:23:35 +0500

    Update app.js

--- app.js
+++ app.js
@@ -49,29 +49,26 @@ search.addEventListener("click", () => {
 
       switch (json.weather[0].main) {
         case "Clear":
-          image.src = "images/clear.png";     
+          image.src = "images/clear.png";
           break;
-          
 
         case "Rain":
           image.src = "images/rain.png";
-        
+
           break;
 
         case "Snow":
           image.src = "images/snow.png";
-          
+
           break;
 
         case "Clouds":
           image.src = "images/cloud.png";
-        
+
           break;
 
         case "Haze":
           image.src = "images/mist.png";
-          bodyElement.style.backgroundImage =
-          "linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12)";
           break;
 
         default:
